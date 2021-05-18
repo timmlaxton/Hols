0816262bf61f86fef05bae62eca7ce1e3c0720bb
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-05-18 16:08:43 +0100

    added video to home page

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Button } from './Button';
+import './HeroSection.css';
 
 const HeroSection = () => {
 	return (
 		<div className="hero-container">
-			<video src="/videos/video-2.mp4" autoPlay loop muted />
+			<video src="/videos/video-4.mp4" autoPlay loop muted />
 			<h1>Adventure awaits</h1>
 			<p>What are you waiting for</p>
 			<div className="hero-btn">
 				<Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
 					Get Started
+				</Button>
+				<Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">
+					Watch trailer
+					<i className="far fa-play-circle" />
 				</Button>
 			</div>
 		</div>
