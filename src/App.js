b2aa82a3523bfd3cb82aeb582bbf3b90ce104181
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-05-18 17:23:45 +0100

    added links to cards

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/screens/Home';
+import Products from './components/screens/Products';
+import Services from './components/screens/Services';
+import SingUp from './components/screens/SignUp';
 
 function App() {
 	return (
@@ -11,6 +14,9 @@ function App() {
 				<Navbar />
 				<Switch>
 					<Route path="/" exact component={Home} />
+					<Route path="/products" exact component={Products} />
+					<Route path="/services" exact component={Services} />
+					<Route path="/sign-up" exact component={SingUp} />
 				</Switch>
 			</Router>
 		</>
---
+++ src/components/screens/SignUp.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../../App.css';
+
+const SingUp = () => {
+	return <h1 className="sign-up">Sign Up</h1>;
+};
+
+export default SingUp;
