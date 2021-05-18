5f876c42d7a4c90f0318ddbffce877b90ce5fa06
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2021-05-18 15:43:19 +0100

    added close mobile to menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 import './Navbar.css';
@@ -20,11 +20,15 @@ const Navbar = () => {
 
 	window.addEventListener('resize', showButton);
 
+	useEffect(() => {
+		showButton();
+	}, []);
+
 	return (
 		<>
 			<nav className="navbar">
 				<div className="navbar-container">
-					<Link to="/" className="navbar-logo">
+					<Link to="/" className="navbar-logo" onClick={closedMobilemenu}>
 						TRVL <i className="fab fa-typo3" />
 					</Link>
 					<div className="menu-icon" onClick={handleClick}>
